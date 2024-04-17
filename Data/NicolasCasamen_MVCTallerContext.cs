7fb1333f83d1caf79a40ac1880dc0090d5e56b8d
laboratorio <laboratorio@UPOAULA10320>
2024-04-17 10:58:32 -0500

    Agregar archivos de proyecto.

---
+++ Data/NicolasCasamen_MVCTallerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NicolasCasamen_MVCTaller.Models;
+
+namespace NicolasCasamen_MVCTaller.Data
+{
+    public class NicolasCasamen_MVCTallerContext : DbContext
+    {
+        public NicolasCasamen_MVCTallerContext (DbContextOptions<NicolasCasamen_MVCTallerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NicolasCasamen_MVCTaller.Models.Burger> Burger { get; set; } = default!;
+    }
+}
