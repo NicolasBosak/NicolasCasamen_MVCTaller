d00534e56f74f201e79a9bc134742f7e66206a98
Sebastián Nicolas Casamen Cuichan <sebastian.casamen@udla.edu.ec>
2024-04-17 11:32:51 -0500

    Clase Promo

--- Models/Promo.cs
+++ Models/Promo.cs
@@ -6,5 +6,6 @@
         public string? Descripcion { get; set;}
         public DateTime FechaPromo { get; set; }
         public int BurgerId { get; set; }
+        public Burger? Burger { get; set; }
     }
 }
